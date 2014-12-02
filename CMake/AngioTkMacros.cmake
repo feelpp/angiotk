9d75ce3922b1ef0e7414a2f5a200bd483cf53ff1
Jerome Velut <jerome.velut@kitware.com>
2014-12-02 21:27:12 +0100

    ENH: Added optional arguments to python-generated component defs
    +comments and macro refactoring

--- CMake/AngioTkMacros.cmake
+++ CMake/AngioTkMacros.cmake
@@ -1,15 +1,44 @@
+# Given the absolute path to a module, this macro set the 
+# AngioTk_CURRENT_MODULE_PATH/NAME variables and cache the 
+# BUILD_MODULE_xxxx user option
+macro( angiotk_module_enablement _modulePath )
+  set( AngioTk_CURRENT_MODULE_PATH ${_modulePath})
+  get_filename_component( moduleName ${_modulePath} NAME)
+  set( AngioTk_CURRENT_MODULE_NAME ${moduleName})
+  set( BUILD_MODULE_${moduleName} FALSE CACHE BOOL "Build module ${moduleName}")
+endmacro()
+
+# Initialisation of a python file containing definitions
+# of components for a given module _moduleName
 macro( angiotk_init_module_python _moduleName )
-  FILE( WRITE ${AngioTk_BINARY_DIR}/${_moduleName}.py
+  file( WRITE ${AngioTk_EXECUTABLE_PATH}/${_moduleName}.py
+        "# AngioTk components for ${_moduleName} module\n"
+        "# This file has been generated automatically by CMake.\n"
+        "# Any change will be overwritten on next Configure.\n"
         "import os\n\n"
       )
 endmacro()
 
+# Create a new AngioTk component:
+#   - Add an executable _target from sources _srcs
+#   - Define a function in the current module python file
+# Note1: This should be called from inside a module (ie 
+# ${AngioTk_CURRENT_MODULE_NAME} should be defined)
+# Note2: the executable should take at least 2 args: 
+#   - an input filename
+#   - an output filename
+# a third argument could optionnally be used to give additional
+# component-specific arguments 
 macro( angiotk_add_component _target _srcs )
-  message( STATUS ${AngioTk_CURRENT_MODULE_NAME}.${_target})
+  message( STATUS "Creating python file for ${AngioTk_CURRENT_MODULE_NAME}.${_target}")
   add_executable( ${_target} ${_srcs} )
-  FILE( APPEND ${AngioTk_BINARY_DIR}/${AngioTk_CURRENT_MODULE_NAME}.py
-        "def ${_target}(input, output):\n"
-        "  os.system(\"${AngioTk_BINARY_DIR}/bin/${_target} \"+input+\" \"+output)\n\n"
+  file( APPEND ${AngioTk_EXECUTABLE_PATH}/${AngioTk_CURRENT_MODULE_NAME}.py
+        "def ${_target}(inputFile, outputFile, componentArgs=\"\"):\n"
+        "  os.system(\"${AngioTk_EXECUTABLE_PATH}/${_target} \"
+                     +inputFile+\" \"
+                     +outputFile+\" \"
+                     +componentArgs
+                    )\n\n"
       )
 endmacro()
 
--- CMake/AngioTkModules.cmake
+++ CMake/AngioTkModules.cmake
@@ -1,27 +1,17 @@
 # this script finds the modules in Modules/ folder 
 # and include the components for each enabled module
-
-macro( AngioTkModuleEnablement _modulePath )
-  SET( AngioTk_CURRENT_MODULE_PATH ${_modulePath})
-  get_filename_component( moduleName ${_modulePath} NAME)
-  SET( AngioTk_CURRENT_MODULE_NAME ${moduleName})
-  SET( BUILD_MODULE_${moduleName} FALSE CACHE BOOL "Build module ${moduleName}")
- 
-  IF( ${BUILD_MODULE_${moduleName}} )
-    angiotk_init_module_python(${moduleName}) 
-    file( GLOB components ${_modulePath}/* ) 
-    foreach( component ${components} )
-      if( IS_DIRECTORY ${component} )
-        ADD_SUBDIRECTORY( ${component} )  
-      endif()
-    endforeach()
-  ENDIF()
-endmacro() 
-
-
 file( GLOB modules ${AngioTk_SOURCE_DIR}/Modules/* ) 
 foreach( module ${modules} )
   if( IS_DIRECTORY ${module} )
-    AngioTkModuleEnablement( ${module} )
+    angiotk_module_enablement( ${module} )
+    if( ${BUILD_MODULE_${AngioTk_CURRENT_MODULE_NAME}} )
+      angiotk_init_module_python(${AngioTk_CURRENT_MODULE_NAME})
+      file( GLOB components ${AngioTk_CURRENT_MODULE_PATH}/* )
+      foreach( component ${components} )
+        if( IS_DIRECTORY ${component} )
+          add_subdirectory( ${component} )
+        endif()
+      endforeach()
+    endif()
   endif()
 endforeach()
