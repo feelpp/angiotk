7e94a9fcb27eb2078889111f8f1fbfc6817d14e3
Vincent Chabannes <vincent.chabannes@feelpp.org>
2015-08-16 10:43:50 +0200

    possibility to use opensurface as a preprocess of remeshstl

--- Modules/Meshing/VolumeFromSTL/src/remeshstl_main.cpp
+++ Modules/Meshing/VolumeFromSTL/src/remeshstl_main.cpp
@@ -7,7 +7,11 @@ int main( int argc, char** argv )
 {
     using namespace Feel;
 
-    po::options_description myoptions = RemeshSTL::options("");
+    po::options_description myoptions;
+    myoptions.add( RemeshSTL::options("") );
+    myoptions.add_options()
+      ("pre-process.open-surface", Feel::po::value<bool>()->default_value(false), "preprocess : open-surface ");
+    myoptions.add( OpenSurface::options("open-surface") );
 
     Environment env( _argc=argc, _argv=argv,
                      _desc=myoptions,
@@ -15,7 +19,31 @@ int main( int argc, char** argv )
 				  _author="Feel++ Consortium",
 				  _email="feelpp-devel@feelpp.org"));
 
+    bool preProcessOpenSurface = boption(_name="pre-process.open-surface");
+
     RemeshSTL myRemeshSTL("");
+    if ( preProcessOpenSurface )
+      {
+	if ( myRemeshSTL.inputCenterlinesPath().empty() )
+	  {
+	    std::cout << "no inputCenterlinesPath -> exit";
+	    return 0;
+	  }
+
+	OpenSurface myOpenSurface("open-surface");
+	myOpenSurface.setInputSurfacePath(myRemeshSTL.inputSurfacePath());
+	myOpenSurface.setInputCenterlinesPath(myRemeshSTL.inputCenterlinesPath());
+
+	myOpenSurface.setOutputDirectory( fs::path(myRemeshSTL.outputPath()).parent_path().string() );
+	myOpenSurface.updateOutputPathFromInputFileName();
+	myOpenSurface.run();
+
+	// update RemeshSTL
+	myRemeshSTL.setInputSurfacePath( myOpenSurface.outputPath() );
+	if ( myOpenSurface.forceRebuild() )
+	  myRemeshSTL.setForceRebuild( true );
+      }
+
     myRemeshSTL.run();
 
     return 0;
