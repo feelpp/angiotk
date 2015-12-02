35664d5b14f6b6aa31545703fcbc7059e44b5448
Vincent Chabannes <vincent.chabannes@feelpp.org>
2015-12-02 10:53:00 +0100

    up Example

---
+++ Examples/MeshFromMRI/Base2_FOA42/veinous/runpipeline.sh
@@ -0,0 +1,12 @@
+
+export ANGIOTK_BINARY_DIR=$HOME/AngioTK/angiotk.build/bin/
+export MY_CFG_DIR=$HOME/AngioTK/angiotk/Examples/MeshFromMRI/Base2_FOA42/veinous/
+
+$ANGIOTK_BINARY_DIR/meshing_surfacefromimage --config-file $MY_CFG_DIR/surfacefromimage.cfg || exit 1
+$ANGIOTK_BINARY_DIR/meshing_centerlines --config-file $MY_CFG_DIR/centerlines.cfg || exit 1
+$ANGIOTK_BINARY_DIR/meshing_centerlinesmanager --config-file $MY_CFG_DIR/centerlinesmanager.cfg || exit 1
+$ANGIOTK_BINARY_DIR/meshing_imagefromcenterlines --config-file $MY_CFG_DIR/imagefromcenterlines.cfg  || exit 1
+$ANGIOTK_BINARY_DIR/meshing_surfacefromimage --config-file $MY_CFG_DIR/surfacefromimage2.cfg || exit 1
+$ANGIOTK_BINARY_DIR/meshing_remeshstl --config-file $MY_CFG_DIR/remeshstlgmsh.cfg || exit 1
+
+# $ANGIOTK_BINARY_DIR/meshing_volumefromstlandcenterlines --config-file $MY_CFG_DIR/volumefromstlandcenterlines.cfg || exit 1
