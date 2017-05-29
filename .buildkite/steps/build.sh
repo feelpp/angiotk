ae7a3acbd74c30cf45b383420a104efaf171233d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-05-29 10:34:40 +0200

    update
     . use updated scripts for dockerization @vincentchabannes
     . use cmake flags

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -6,7 +6,7 @@ set -x
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Dockerizing $PROJECT..."
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -e GITHUB_OAUTH  feelpp/feelpp-libs:develop-ubuntu-17.04 \
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -e GITHUB_OAUTH  feelpp/feelpp-libs:develop-ubuntu-16.10 \
        sudo \
        PROJECT=${PROJECT} \
        GITHUB_OAUTH=$GITHUB_OAUTH \
@@ -16,4 +16,5 @@ docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -e GITHUB_OAUTH
        BRANCH=${BRANCH} \
        FEELPP_BRANCH=${FEELPP_BRANCH} \
        BUILD_JOBS=${BUILD_JOBS} \
+       CMAKE_FLAGS=${CMAKE_FLAGS} \
        feelpp_dockerize.sh
