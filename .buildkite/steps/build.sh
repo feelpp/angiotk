5334409dd7c5efc6ccfbfc9ea48fee39dd23042e
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-05-29 10:39:10 +0200

    now option are passed via arguments

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -8,13 +8,6 @@ BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 echo "--- Dockerizing $PROJECT..."
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -e GITHUB_OAUTH  feelpp/feelpp-libs:develop-ubuntu-16.10 \
        sudo \
-       PROJECT=${PROJECT} \
        GITHUB_OAUTH=$GITHUB_OAUTH \
        CXX=${CXX} \
-       TARGET=${TARGET} \
-       FROM=${FROM} \
-       BRANCH=${BRANCH} \
-       FEELPP_BRANCH=${FEELPP_BRANCH} \
-       BUILD_JOBS=${BUILD_JOBS} \
-       CMAKE_FLAGS=${CMAKE_FLAGS} \
-       feelpp_dockerize.sh
+       feelpp_dockerize.sh -p=${PROJECT} -t=${TARGET} -c="${CMAKE_FLAGS}" -f=${FROM} -j=${BUILD_JOBS} -b=${BRANCH} --feelpp-branch=${FEELPP_BRANCH}
