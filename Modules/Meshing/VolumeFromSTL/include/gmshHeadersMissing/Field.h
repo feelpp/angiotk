1b609409e20614b8e922911464f82e84a39dedd5
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-02-20 15:32:20 +0100

    Revert "add empty docs dir"
    This reverts commit 1ec95ba7b62fcf42bde592d8d970bcdf342266bd.

--- Modules/Meshing/VolumeFromSTL/include/gmshHeadersMissing/Field.h
+++ Modules/Meshing/VolumeFromSTL/include/gmshHeadersMissing/Field.h
@@ -1,14 +1,13 @@
-// Gmsh - Copyright (C) 1997-2017 C. Geuzaine, J.-F. Remacle
+// Gmsh - Copyright (C) 1997-2014 C. Geuzaine, J.-F. Remacle
 //
 // See the LICENSE.txt file for license information. Please report all
-// bugs and problems to the public mailing list <gmsh@onelab.info>.
+// bugs and problems to the public mailing list <gmsh@geuz.org>.
 
 #ifndef _FIELD_H_
 #define _FIELD_H_
 
 #include <string>
 #include <map>
-#include <vector>
 #include <list>
 #include "GmshConfig.h"
 #include "STensor3.h"
@@ -31,8 +30,7 @@ typedef enum {
   FIELD_OPTION_STRING,
   FIELD_OPTION_PATH,
   FIELD_OPTION_BOOL,
-  FIELD_OPTION_LIST,
-  FIELD_OPTION_LIST_DOUBLE
+  FIELD_OPTION_LIST
 } FieldOptionType;
 
 class FieldCallback {
@@ -65,23 +63,20 @@ class FieldOption {
     case FIELD_OPTION_PATH: return "path"; break;
     case FIELD_OPTION_STRING: return "string"; break;
     case FIELD_OPTION_LIST: return "list"; break;
-    case FIELD_OPTION_LIST_DOUBLE: return "list_double"; break;
     default: return "unknown";
     }
   }
   virtual void numericalValue(double val) { throw(1); }
   virtual double numericalValue() const { throw(1); }
-  virtual const std::list<int> &list() const { printf("coucou4\n");throw(1); }
-  virtual const std::list<double> &listdouble() const { printf("coucou3\n");throw(1); }
-  virtual void list(std::list<int> value) { printf("coucou1\n");throw(1); }
-  virtual void listdouble(std::list<double> value) { printf("coucou2\n");throw(1); }
+  virtual const std::list<int> &list() const { throw(1); }
+  virtual void list(std::list<int> value) { throw(1); }
   virtual std::string string() const { throw(1); }
   virtual void string(const std::string value) { throw(1); }
 };
 
 class Field {
  public:
-  Field() : update_needed(false) {}
+  Field() {}
   virtual ~Field();
   int id;
   std::map<std::string, FieldOption *> options;
@@ -91,10 +86,12 @@ class Field {
   virtual double operator() (double x, double y, double z, GEntity *ge=0) = 0;
   // anisotropic
   virtual void operator() (double x, double y, double z, SMetric3 &, GEntity *ge=0){}
-  // temporary
-  virtual void operator()(double x, double y, double z, SVector3& v1, SVector3& v2,
-                          SVector3& v3,GEntity* ge=0){}
+
+  //temporary
+  virtual void operator()(double x,double y,double z,SVector3& v1,SVector3& v2,SVector3& v3,GEntity* ge=0){}
+
   bool update_needed;
+  //void update(){ printf("up f \n"); return;}
   virtual const char *getName() = 0;
 #if defined(HAVE_POST)
   void putOnView(PView * view, int comp = -1);
@@ -143,13 +140,12 @@ class AttractorField;
 class BoundaryLayerField : public Field {
  private:
   std::list<AttractorField *> _att_fields;
-  std::list<double> hwall_n_nodes;
-  std::list<int> nodes_id, edges_id;
-  std::list<int> edges_id_saved, nodes_id_saved, fan_nodes_id;
+  std::list<int> nodes_id, edges_id, faces_id;
+  std::list<int> faces_id_saved, edges_id_saved, nodes_id_saved, fans_id, fan_nodes_id;
   void operator() (AttractorField *cc, double dist, double x, double y, double z,
                    SMetric3 &metr, GEntity *ge);
  public:
-  double hwall_n,ratio,hfar,thickness,fan_angle;
+  double hwall_n,hwall_t,ratio,hfar,thickness,fan_angle;
   double current_distance, tgt_aniso_ratio;
   SPoint3 _closest_point;
   int iRecombine, iIntersect;
@@ -161,6 +157,10 @@ class BoundaryLayerField : public Field {
   ~BoundaryLayerField() {removeAttractors();}
   virtual double operator() (double x, double y, double z, GEntity *ge=0);
   virtual void operator() (double x, double y, double z, SMetric3 &metr, GEntity *ge=0);
+  bool isFaceBL (int iF) const
+  {
+    return std::find(faces_id.begin(),faces_id.end(),iF) != faces_id.end();
+  }
   bool isEdgeBL (int iE) const
   {
     return std::find(edges_id.begin(),edges_id.end(),iE) != edges_id.end();
@@ -170,26 +170,21 @@ class BoundaryLayerField : public Field {
     return std::find(edges_id_saved.begin(),edges_id_saved.end(),iE)
       != edges_id_saved.end();
   }
+  bool isFan (int iE) const
+  {
+    return std::find(fans_id.begin(),fans_id.end(),iE) != fans_id.end();
+  }
   bool isFanNode (int iV) const
   {
     return std::find(fan_nodes_id.begin(),fan_nodes_id.end(),iV) != fan_nodes_id.end();
   }
-  bool isEndNode (int iV) const
+  bool isVertexBL (int iV) const
   {
     return std::find(nodes_id.begin(),nodes_id.end(),iV) != nodes_id.end();
   }
-  double hwall (int iV){
-    for (std::list<double>::iterator it = hwall_n_nodes.begin(); it != hwall_n_nodes.end(); ++it){
-      int i = (int) *it; ++it;
-      double h = *it;
-      if (i == iV)return h;
-    }
-    return hwall_n;
-  }
-
   void computeFor1dMesh(double x, double y, double z, SMetric3 &metr);
-  void setupFor1d(int iE);
   void setupFor2d(int iF);
+  void setupFor3d();
   void removeAttractors();
 };
 
@@ -294,31 +289,6 @@ class FieldOptionList : public FieldOption
   }
 };
 
-class FieldOptionListDouble : public FieldOption
-{
- public:
-  std::list<double> &val;
-  FieldOptionType getType(){ return FIELD_OPTION_LIST_DOUBLE; }
-  FieldOptionListDouble(std::list<double> &_val, std::string _help, bool *_status=0)
-    : FieldOption(_help, _status), val(_val) {}
-  void listdouble(std::list<double> value){ modified(); val = value; }
-  const std::list<double>& listdouble() const { return val; }
-  void getTextRepresentation(std::string & v_str)
-  {
-    std::ostringstream sstream;
-    sstream.precision(16);
-    sstream << "{";
-    for(std::list<double>::iterator it = val.begin(); it != val.end(); it++) {
-      if(it != val.begin())
-        sstream << ", ";
-      sstream << *it;
-    }
-    sstream << "}";
-    v_str = sstream.str();
-  }
-};
-
-
 class FieldOptionPath : public FieldOptionString
 {
  public:
@@ -364,29 +334,6 @@ class FieldCallbackGeneric : public FieldCallback {
 template<class F> class FieldFactoryT : public FieldFactory {
  public:
   Field * operator()() { return new F; }
-};
-
-
-
-// the class GenericField contains a set of void* functions, which give a mesh size
-// All these functions are called when calling operator() ; then, the minimum size is returned.
-class GenericField : public Field{
-  public:
-    // callback prototypes:
-    // this callback is called with a void* previously given to the GenericField !
-    typedef bool (*ptrfunction)(double, double, double, void*, double&);
-
-    GenericField();
-    ~GenericField();
-    virtual double operator() (double x, double y, double z, GEntity *ge=0);
-    virtual const char *getName(){return "GenericField";};
-
-    // sets the callbacks
-    void setCallbackWithData(ptrfunction fct, void *data);
-
-  private:
-    std::vector<ptrfunction> cbs;// the callbacks
-    std::vector<void*> user_data;// the data to be sent to the callbacks
 };
 
 #endif
--- Modules/Meshing/VolumeFromSTL/include/gmshHeadersMissing/meshGRegion.h
+++ Modules/Meshing/VolumeFromSTL/include/gmshHeadersMissing/meshGRegion.h
@@ -1,7 +1,7 @@
-// Gmsh - Copyright (C) 1997-2017 C. Geuzaine, J.-F. Remacle
+// Gmsh - Copyright (C) 1997-2014 C. Geuzaine, J.-F. Remacle
 //
 // See the LICENSE.txt file for license information. Please report all
-// bugs and problems to the public mailing list <gmsh@onelab.info>.
+// bugs and problems to the public mailing list <gmsh@geuz.org>.
 
 #ifndef _MESH_GREGION_H_
 #define _MESH_GREGION_H_
@@ -35,13 +35,13 @@ class meshGRegionExtruded {
 // Optimize the mesh of the region using gmsh's algo
 class optimizeMeshGRegionGmsh {
  public :
-  void operator () (GRegion *, bool always=false);
+  void operator () (GRegion *);
 };
 
 // Optimize the mesh of the region using netgen's algo
 class optimizeMeshGRegionNetgen {
  public :
-  void operator () (GRegion *, bool always=false);
+  void operator () (GRegion *);
 };
 
 // destroy the mesh of the region
